fb49c6f7162af182463514a63c4826a96a8a515f
elaynne torres <elaynne.ufs@gmail.com>
2019-04-21 00:06:19 -0300

    Stack of int

---
+++ SortLib/Collections/Stack.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace SortLib.Collections
+{
+    public class StackNode
+    {
+        public StackNode Next { get; set; }
+        public int Element { get; set; }
+
+        public StackNode(StackNode next, int element)
+        {
+            Next = next;
+            Element = element;
+        }
+    }
+
+    public class Stack
+    {
+        public StackNode Top { get; set; }
+
+        public StackNode GetTop() => Top;
+        
+        public void Push(int element)
+        {
+            if (Top == null)
+                Top = new StackNode(null, element);
+            else
+            {
+                StackNode aux = new StackNode(Top, element);
+                Top = aux;
+            }
+        }
+
+        public StackNode Pop()
+        {
+            if (Top != null)
+            {
+                StackNode aux = Top;
+                Top = Top.Next;
+                aux.Next = null;
+                return aux;
+            }
+            else
+                return null;
+        }
+
+        public void PrintStack()
+        {
+            StackNode aux = Top;
+            StringBuilder result = new StringBuilder();
+            while (aux != null)
+            {
+                result.Append((aux.Element).ToString() + "\n");
+                aux = aux.Next;
+            }
+        }
+    }
+}
